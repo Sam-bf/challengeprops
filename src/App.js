00bfcbb41d3673cf952f625d3293bf62b65e7fba
samy bf <sbfadhloun@gmail.com>
2023-11-27 12:08:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Intro from './Intro';
+import Projectlist from './Projectlist';
+import Contact from './Contact';
+import Footer from './Footer';
 
 function App() {
+  var name="John ben fadhloun"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar n={name}/>
+    <Intro n={name}/>
+    <Projectlist/>
+    <Contact/>
+    <Footer/>
+    </>
   );
 }
 
